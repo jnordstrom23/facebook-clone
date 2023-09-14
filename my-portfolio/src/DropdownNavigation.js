feec9abc84176f2dab78942ff9a073a7b3a42ddb
jnordstrom23 <jeffnordstrom84@gmail.com>
2023-09-14 09:48:39 -0700

    Saved editor

--- my-portfolio/src/DropdownNavigation.js
+++ my-portfolio/src/DropdownNavigation.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import "./DropdownNavigation.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
-
 import { Link } from 'react-router-dom';
 
 
