f3ac3ac12996e7dbb379c10407ad122798ea36f5
jnordstrom23 <jeffnordstrom84@gmail.com>
2023-09-14 09:45:25 -0700

    Project page initial layout and css

--- my-portfolio/src/Projects.js
+++ my-portfolio/src/Projects.js
@@ -1,5 +1,28 @@
+import "./Projects.css";
+
+
 function Projects() {
-    return <h1>Projects</h1>;
-  };
+  return (
+    <div className="Home">
+    <div class="row">
+    
+    
+    <div class="column">
+    <h2 className="Project_Title">Restuarant App</h2>
+    
+
+</div>
+
+
+
+    <div class="column">
+    <h2 className="Project_Title">Facebook Clone</h2>
+</div>
+</div>
+</div>
+
+
+)
+}
   
   export default Projects;
